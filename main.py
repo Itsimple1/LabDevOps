8c1b084b5a96fb1fed4c36c5b6f261d982b1f730
Itsimple1 <gsn.perevoz@yandex.ru>
2023-06-01 22:44:30 +0300

    Add tests

--- main.py
+++ main.py
@@ -4,33 +4,32 @@ import time
 sw = 0
 
 
-def answer(com):
+def answer(command):
     global sw
-    match com:
+    match command:
         case "start":
             sw = time.time()
-            print("The stopwatch is running")
+            return "The stopwatch is running"
         case "stop":
             if sw == 0:
-                print("The stopwatch is not running")
+                return "The stopwatch is not running"
             else:
-                print(time.strftime("%H:%M:%S", time.gmtime(int(time.time() - sw))))
+                return time.strftime("%H:%M:%S", time.gmtime(int(time.time() - sw)))
                 sw = 0
         case "now":
-            print(datetime.datetime.now().time())
+            return datetime.datetime.now().time()
         case "--help":
-            print("List of commands to control:\n" +
-                  "start - start the stopwatch\n" +
-                  "stop - stop the stopwatch and get the time\n" +
-                  "now - the time is now\n" +
-                  "exit - command for exit\n" +
-                  "--help - info about command\n")
+            return ("List of commands to control:\n" +
+                    "start - start the stopwatch\n" +
+                    "stop - stop the stopwatch and get the time\n" +
+                    "now - the time is now\n" +
+                    "exit - command for exit\n" +
+                    "--help - info about command\n")
         case _:
-            print("Command not found")
+            return "Command not found"
 
 
 def main():
-
     print("Hi, this is an application which stimulates the stopwatch\n\n" +
           "List of commands to control:\n" +
           "start - start the stopwatch\n" +
@@ -41,11 +40,11 @@ def main():
 
     command = input(">> ")
     while True:
-        match com:
+        match command:
             case "exit":
                 break
             case _:
-                answer(com)
+                print(answer(command))
         command = input(">> ")
 
 
