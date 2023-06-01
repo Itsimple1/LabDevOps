a63e8bb09c0112d0b3fff6108d7b6713ed804e14
Itsimple1 <gsn.perevoz@yandex.ru>
2023-06-01 23:25:18 +0300

    Fix and add analise code

--- main.py
+++ main.py
@@ -19,25 +19,28 @@ def answer(com):
         case "now":
             return datetime.datetime.now().time()
         case "--help":
-            return ("List of commands to control:\n" +
-                  "start - start the stopwatch\n" +
-                  "stop - stop the stopwatch and get the time\n" +
-                  "now - the time is now\n" +
-                  "exit - command for exit\n" +
-                  "--help - info about command\n")
+            return (
+                "List of commands to control:\n"
+                + "start - start the stopwatch\n"
+                + "stop - stop the stopwatch and get the time\n"
+                + "now - the time is now\n"
+                + "exit - command for exit\n"
+                + "--help - info about command\n"
+            )
         case _:
             return "Command not found"
 
 
 def main():
-
-    print("Hi, this is an application which stimulates the stopwatch\n\n" +
-          "List of commands to control:\n" +
-          "start - start the stopwatch\n" +
-          "stop - stop the stopwatch and get the time\n" +
-          "now - the time is now\n" +
-          "exit - command for exit\n" +
-          "--help - info about command\n\n")
+    print(
+        "Hi, this is an application which stimulates the stopwatch\n\n"
+        + "List of commands to control:\n"
+        + "start - start the stopwatch\n"
+        + "stop - stop the stopwatch and get the time\n"
+        + "now - the time is now\n"
+        + "exit - command for exit\n"
+        + "--help - info about command\n\n"
+    )
 
     command = input(">> ")
     while True:
--- test_main.py
+++ test_main.py
@@ -4,14 +4,21 @@ from main import answer
 
 
 @pytest.mark.parametrize(
-    ('command', 'output'),
-    [('start', 'The stopwatch is running'),
-     ('--help', ("List of commands to control:\n" +
-                 "start - start the stopwatch\n" +
-                 "stop - stop the stopwatch and get the time\n" +
-                 "now - the time is now\n" +
-                 "exit - command for exit\n" +
-                 "--help - info about command\n"))]
+    ("command", "output"),
+    [
+        ("start", "The stopwatch is running"),
+        (
+            "--help",
+            (
+                "List of commands to control:\n"
+                + "start - start the stopwatch\n"
+                + "stop - stop the stopwatch and get the time\n"
+                + "now - the time is now\n"
+                + "exit - command for exit\n"
+                + "--help - info about command\n"
+            ),
+        ),
+    ],
 )
 def test_answer(command, output):
     assert answer(command) == output
