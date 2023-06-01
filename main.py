76cb3a95955ce36a7277fad06152ba48fde91e94
Itsimple1 <gsn.perevoz@yandex.ru>
2023-06-01 19:45:54 +0300

    Fix problem

--- main.py
+++ main.py
@@ -1,33 +1,53 @@
-import asyncio
-import logging
+import datetime
+import time
 
-from aiogram import Bot, Dispatcher, types
-
-import config
-
-logging.basicConfig(level=logging.INFO)
-
-# Bot assignment--------------------------------------------------------------------------------------------------------
-bot = Bot(token=config.TOKEN)
-dp = Dispatcher()
+sw = 0
 
 
-@dp.message(commands="start")
-async def cmd_start(message: types.Message):
-    await message.answer(
-        "Привет, это эхо бот. Отправь любое сообщение и бот пришлет тебе такое же в ответ)"
-    )
+def answer(command):
+    global sw
+    match command:
+        case "start":
+            sw = time.time()
+            print("The stopwatch is running")
+        case "stop":
+            if sw == 0:
+                print("The stopwatch is not running")
+            else:
+                print(time.strftime("%H:%M:%S", time.gmtime(int(time.time() - sw))))
+                sw = 0
+        case "now":
+            print(datetime.datetime.now().time())
+        case "--help":
+            print("List of commands to control:\n" +
+                  "start - start the stopwatch\n" +
+                  "stop - stop the stopwatch and get the time\n" +
+                  "now - the time is now\n" +
+                  "exit - command for exit\n" +
+                  "--help - info about command\n")
+        case _:
+            print("Command not found")
 
 
-@dp.message()
-async def with_puree(message: types.Message):
-    await message.answer(message.text)
+def main():
 
+    print("Hi, this is an application which stimulates the stopwatch\n\n" +
+          "List of commands to control:\n" +
+          "start - start the stopwatch\n" +
+          "stop - stop the stopwatch and get the time\n" +
+          "now - the time is now\n" +
+          "exit - command for exit\n" +
+          "--help - info about command\n\n")
 
-async def main():
-    await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
+    command = input(">> ")
+    while True:
+        match command:
+            case "exit":
+                break
+            case _:
+                answer(command)
+        command = input(">> ")
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
