c4b95f843f2177cb6f896e7edf04370b677be06d
itsimple <90086457+Itsimple1@users.noreply.github.com>
2023-06-16 00:43:45 +0300

    Create im.py

---
+++ im.py
@@ -0,0 +1,6 @@
+print(
+        "Hi, this is an application which stimulates the stopwatch\n\n"
+        + "This application allows you to measure the time between \n"
+        + "intervals, find out the time now, and also get help for \n"
+        + "commands on this application"
+    )
